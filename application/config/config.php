025198e60d96ae04675648b9ddb4bd3236820e9e
gchamounster <gchamounster123@gmail.com>
2018-12-04 10:15:27 -0600

    transfer work bt computers

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.1.213/';
+$config['base_url'] = 'http://172.31.145.237/';
 /*
 |--------------------------------------------------------------------------
 | Index File
